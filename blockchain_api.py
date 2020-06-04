407f86be4441a6ccc23fa7208958b427a147ce66
Emeka Ugwuanyi <emylincon@gmail.com>
2020-06-04 15:23:04 +0100

    added testing data, user and admin rights, mining functionality and more

--- blockchain_api.py
+++ blockchain_api.py
@@ -5,12 +5,14 @@ import ast
 import json
 from users import Data
 from functools import wraps
+import config
 
 
 app = Flask(__name__)
 api = Api(app)                # initializing app
-chain = BlockChain()          # initializing block chain
 store = Data()                # initializing user data
+admin = store.get_key(**config.test)     # creating super user
+chain = BlockChain(admin)          # initializing block chain
 
 
 def auth_required(f):           # user verification authentication
@@ -55,9 +57,10 @@ class AddBlock(Resource):
     @staticmethod
     @auth_required
     def post():
+        user = store.get_key(request.authorization.username, request.authorization.password)
         sent_data = request.get_json()
         if sent_data != '':
-            return chain.add_block(sent_data), 201
+            return chain.add_block(sent_data, user), 201
         else:
             return {'error': 'no data sent'}
 
@@ -66,18 +69,25 @@ class Read(Resource):
     @staticmethod
     @auth_required
     def get(text):
+        user = store.get_key(request.authorization.username, request.authorization.password)
+        name = {'user': request.authorization.username}
         if text == 'all':           # reads all data in block chain
-            return json.dumps(chain.read_all())
+            response = chain.read_all(user)
+            if type(response).__name__ == 'list':
+                response.append(name)
+            return json.dumps(response)
         else:
             try:
-                data = ast.literal_eval(text)
+                data = ast.literal_eval(text)      # converts data to dictionary
                 if type(data).__name__ == 'dict':
-                    a = chain.read_block(**data)
-                    return json.dumps(a)         # reads a particular block with nonce id or hash
+                    response = chain.read_block(**data, user=user)
+                    if type(response).__name__ == 'list':
+                        response.append(name)
+                    return json.dumps(response)         # reads a particular block with nonce id or hash
                 else:
-                    return {'error': 'wrong format -> Example -> {nonce: 1} or {hash_: 127hdwu861eh}'}
+                    return json.dumps({'error': 'wrong format -> Example -> {nonce: 1} or {hash_: 127hdwu861eh}'})
             except Exception as e:
-                return {'error': e}
+                return json.dumps({'error': str(e)})
 
 
 api.add_resource(HomePage, '/')
