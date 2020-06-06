caff8a435d24ee41d2d0d00a3f62d63c7a6a105d
Emeka Ugwuanyi <emylincon@gmail.com>
2020-06-06 08:35:44 +0100

    commit

--- request_from_api.py
+++ request_from_api.py
@@ -2,14 +2,18 @@ import requests
 import json
 import config
 
-url = 'http://127.0.0.1:5000/'
+url = 'http://192.168.40.169:5000/'
 auth = ('username', 'password')
 auth1 = ('john', 'pass')
 
 
 def get_data(endpoint='', auth_=None):
     response = requests.get(url + endpoint, auth=auth_, )
-    data = json.loads(response.content)
+    try:
+        data = json.loads(response.content)
+    except Exception as e:
+        data = response.content
+
     print(data)
     return data
 
@@ -17,14 +21,17 @@ def get_data(endpoint='', auth_=None):
 def post_data(data, auth_=None, endpoint=''):
     # response = requests.post(url+endpoint, data='mama', json=data)
     response = requests.post(url + endpoint, json=data, auth=auth_, )
-    data = json.loads(response.content)
-    print(data)
-    return data
+    try:
+        d = json.loads(response.content)
+    except Exception as e:
+        d = response.content
+    print(d)
+    return d
 
 
-post_data(endpoint='register/', data={"user": "john", "pw": "pass"})
-response = post_data(endpoint='add/', data={'name': 'emeka'}, auth_=auth1)
-get_data(endpoint='read/{"nonce":' + f'{response["nonce"]}' + '}', auth_=auth1)
-get_data(endpoint='read/{"hash_":' + f'"{response["hash"]}"' + '}', auth_=auth1)
-get_data(endpoint='read/all', auth_=auth1)
+# post_data(endpoint='register/', data={"user": "john", "pw": "pass"})
+# response = post_data(endpoint='add/', data={'name': 'emeka'}, auth_=auth1)
+# get_data(endpoint='read/{"nonce":' + f'{response["nonce"]}' + '}', auth_=auth1)
+# get_data(endpoint='read/{"hash_":' + f'"{response["hash"]}"' + '}', auth_=auth1)
+# get_data(endpoint='read/all', auth_=auth1)
 get_data(endpoint='read/all', auth_=config.t)
