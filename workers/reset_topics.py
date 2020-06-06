5d96132cd79e656c4f34a04b99eb8ba09bca3ac9
Emeka Ugwuanyi <emylincon@gmail.com>
2020-06-06 16:01:33 +0100

    reset

--- workers/reset_topics.py
+++ workers/reset_topics.py
@@ -1,7 +1,7 @@
 import os
 
 topics = ['blockchain/worker/chain', 'blockchain/worker/mine', 'blockchain/worker/add', 'blockchain/worker/times',
-          'blockchain/api/block_winner', 'blockchain/api/notification']
+          'blockchain/api/block_winner', 'blockchain/api/notification', 'blockchain/config']
 
 for topic in topics:
     cmd = f"mosquitto_pub -h localhost -t {topic} -u admin -P password -n -r -d"
