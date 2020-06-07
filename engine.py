b7c1c058467e4103a90ffe7b6fd2531636b3a43e
Emeka Ugwuanyi <emylincon@gmail.com>
2020-06-07 12:55:23 +0100

    central engine

--- engine.py
+++ engine.py
@@ -1,6 +1,31 @@
 import datetime
 import hashlib
 
+due_contracts = []  # {contract_id : block to add}
+contracts = {}  # contract_id : contract
+
+
+class SmartContract:
+    def __init__(self, creator, goal, con_id, users):
+        self.creator = creator
+        self.users = users
+        self.goal = goal
+        self.con_id = con_id
+
+    def rule(self):  # contract pays all users 300 after data reaches 3, goal is decremented by donation
+        if self.goal <= 0:
+            for user in self.users + [self.creator]:
+                due_contracts.append({self.con_id: {'data':
+                                                        {f"{','.join(self.users)} received": 300,
+                                                         'contract': self.contract_info()},
+                                                    'user': user}})
+
+    def contract_info(self):
+        return {'contract_id': self.con_id, 'creator': self.creator, 'users': self.users}
+
+    def __del__(self):
+        print('Contract Terminated')
+
 
 class Block:
 
@@ -37,15 +62,16 @@ class Block:
 
 class BlockChain:
     def __init__(self, admin):
-        self.diff = 2                      # diff is set to 2, diff controls how easy it is to mine a block
-        self.max_nonce = 2**32             # the max nonce that a block can have
-        self.chain = []   # initializing chain with a genesis block
+        self.diff = 2  # diff is set to 2, diff controls how easy it is to mine a block
+        self.max_nonce = 2 ** 32  # the max nonce that a block can have
+        self.chain = []  # initializing chain with a genesis block
         self.admin = admin
+        self.con_id = 1
         self.add_genesis(admin)
 
     def add_genesis(self, admin):
         if len(self.chain) == 0:
-            self.chain.append(self.mine_block(Block('Genesis', nonce=1, user=admin)),)
+            self.chain.append(self.mine_block(Block('Genesis', nonce=1, user=admin)), )
 
     @property
     def diff_string(self):
@@ -54,19 +80,44 @@ class BlockChain:
     def get_last_block(self):
         return self.chain[-1]
 
+    def add_contract(self, user, goal, users):
+        contracts[self.con_id] = SmartContract(user, goal, self.con_id, users)
+        self.con_id += 1
+
+    def check_due_contracts(self):
+        global due_contracts
+        d = set()
+        if len(due_contracts) > 0:
+            for data_id in due_contracts:
+                key = list(data_id.keys())[0]
+                self.add_block(**data_id[key])
+                d.add(key)
+            for i in d:
+                del contracts[i]
+            due_contracts = []
+
     def add_block(self, data, user):
         chain_copy = self.chain[:]
-        new_block = Block(data, previous_hash=self.get_last_block().hash, nonce=1, user=user)   # to add a block, first the block is initialised with nonce 1
+        if 'contract' in data:
+            try:
+                self.add_contract(**data['contract'])
+            except Exception as e:
+                return {'error': f'engine cannot add contract, {e}'}
+        new_block = Block(data, previous_hash=self.get_last_block().hash, nonce=1,
+                          user=user)  # to add a block, first the block is initialised with nonce 1
         mined_block = self.mine_block(new_block)  # here block is mined, mining is explained in the mine function
         self.chain.append(mined_block)
         if self.is_chain_valid():
+            self.check_due_contracts()
             return {'info': 'block added successfully', 'nonce': self.get_last_block().nonce,
                     'hash': self.get_last_block().hash}
+
         else:
             self.chain = chain_copy[:]
             return {'info': 'Error detected, block not added'}
 
-    def is_chain_valid(self):        # checks if chain has been compromised, this is called after each addition to chain
+
+    def is_chain_valid(self):  # checks if chain has been compromised, this is called after each addition to chain
         for i in range(1, len(self.chain)):
             prevb = self.chain[i - 1]
             currb = self.chain[i]
@@ -88,18 +139,18 @@ class BlockChain:
             else:
                 block.nonce += 1
 
-    def read_block(self, user, nonce=None, hash_=None):   # reading data stored in the block
+    def read_block(self, user, nonce=None, hash_=None):  # reading data stored in the block
         if not nonce and not hash_:
             return {'error': 'please specify hash or nonce'}
         elif nonce:
             nones = []
-            for block in self.chain:          # checks if nonce in chain
+            for block in self.chain:  # checks if nonce in chain
                 if (block.nonce == nonce) and (block.user == user):
                     nones.append(block.block_info())
             return nones
         elif hash_:
             nones = []
-            for block in self.chain:          # checks if hash in chain
+            for block in self.chain:  # checks if hash in chain
                 if (block.hash == hash_) and (block.user == user):
                     nones.append(block.block_info())
                     return nones
@@ -107,7 +158,7 @@ class BlockChain:
 
     def read_all(self, user):
         if user == self.admin:
-            return [block.block_info() for block in self.chain]   # reads all data in chain
+            return [block.block_info() for block in self.chain]  # reads all data in chain
         else:
             return [block.block_info() for block in self.chain if block.user == user]
 
