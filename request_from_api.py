0618d6d2105ec659d65a624dd0b83333a9890c07
Emeka E Ugwuanyi <emylincon@gmail.com>
2023-06-23 08:42:17 +0100

    update requirements (#15)
    * update requirements
    
    * update requirements.txt

--- request_from_api.py
+++ request_from_api.py
@@ -3,13 +3,16 @@ import json
 import config
 import ast
 
-url = 'http://127.0.0.1:5000/'
-auth = ('username', 'password')
-auth1 = ('john', 'pass')
+url = "http://localhost:5000/"
+auth = ("username", "password")
+auth1 = ("john", "pass")
 
 
-def get_data(endpoint='', auth_=None):
-    response = requests.get(url + endpoint, auth=auth_, )
+def get_data(endpoint="", auth_=None):
+    response = requests.get(
+        url + endpoint,
+        auth=auth_,
+    )
     try:
         data = json.loads(response.content)
     except Exception as e:
@@ -19,27 +22,34 @@ def get_data(endpoint='', auth_=None):
     return data
 
 
-def post_data(data, auth_=None, endpoint=''):
+def post_data(data, auth_=None, endpoint=""):
     # response = requests.post(url+endpoint, data='mama', json=data)
-    response = requests.post(url + endpoint, json=data, auth=auth_, )
+    response = requests.post(
+        url + endpoint,
+        json=data,
+        auth=auth_,
+    )
+    d = ""
     try:
-        d = json.loads(response.content)
+        d = response.json()
+        print(d)
     except Exception as e:
-        d = response.content
-    print(d)
+        print(
+            f"Error with post data ({response.status_code}): {e} = {response.content}"
+        )
+
     return d
 
 
-post_data(endpoint='register/', data={"user": "john", "pw": "pass"})
-res = post_data(endpoint='add/', data={'name': 'emeka'}, auth_=auth1)
-print('type', type(res))
-if type(res).__name__ == 'str':
+post_data(endpoint="register/", data={"user": "john", "pw": "pass"})
+res = post_data(endpoint="add/", data={"name": "emeka"}, auth_=auth1)
+print("type", type(res))
+if type(res).__name__ == "str":
     res = ast.literal_eval(res)
 req1 = {"nonce": res["nonce"]}
-get_data(endpoint=f'read/{req1}', auth_=auth1)
+get_data(endpoint=f"read/{req1}", auth_=auth1)
 req2 = {"hash_": res["hash"]}
-get_data(endpoint=f'read/{req2}', auth_=auth1)
-get_data(endpoint='read/all', auth_=auth1)
-get_data(endpoint='read/all', auth_=config.t)
+get_data(endpoint=f"read/{req2}", auth_=auth1)
+get_data(endpoint="read/all", auth_=auth1)
+get_data(endpoint="read/all", auth_=config.t)
 get_data()
-
